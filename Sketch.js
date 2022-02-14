bdafc55c6ab4d94deec71362bef0263c842bc11b
Yashmita Sharma <71992108+xyashmitax@users.noreply.github.com>
2022-02-14 13:06:29 -0500

    Create Sketch.js

---
+++ Sketch.js
@@ -0,0 +1,23 @@
+var player
+var blockes
+
+
+function preload(){
+	
+}
+
+
+function setup() {
+	createCanvas(500.500);
+
+	//Create the Bodies Here.
+	
+  playerhuman = new Player(10,10,450, 250);
+  
+}
+
+
+function draw() {
+
+}
+
